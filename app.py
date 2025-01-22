95cecc18e18e3d4fc9d131c011eae05876e1813f
Eugene Ciurana <github@cime.net>
2025-01-22 13:26:18 -0800

    Run ssscoring.app 01.

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 # See: https://github.com/pr3d4t0r/SSSCoring/blob/master/LICENSE.txt
 
 
-import streamlit as st
+from ssscoring.app import main
 
-# from ssscoring.app import main
+import streamlit as st
 
 
 if '__main__' == __name__:
     st.write('Hello, world!')
-    # main()
+    main()
 
