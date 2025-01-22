bc634034eb034884e4e611fa12867ea146893c40
Eugene Ciurana <github@cime.net>
2025-01-22 13:20:10 -0800

    Initial deployment test.

---
+++ app.py
@@ -0,0 +1,12 @@
+# See: https://github.com/pr3d4t0r/SSSCoring/blob/master/LICENSE.txt
+
+
+# import streamlit as st
+
+from ssscoring.app import main
+
+
+if '__main__' == __name__:
+    st.write('Hello, world!')
+    # main()
+
