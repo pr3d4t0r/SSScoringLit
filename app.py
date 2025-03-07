8c97fc4bd8f8c40e41c7fa284fdbe4a999686a56
Eugene Ciurana <github@cime.net>
2025-03-07 06:53:42 -0800

    Deploy 2.0.29 Bert testing.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 
 from ssscoring import __VERSION__
-from ssscoring.ssscoremultiple import main as score_multilple_jumps
+# from ssscoring.ssscoremultiple import main as score_multilple_jumps
 from ssscoring.ssscoresingle import main as score_single_jump
 
 import streamlit as st
@@ -11,8 +11,9 @@ import streamlit as st
 if '__main__' == __name__:
     st.set_page_config(layout = 'wide', page_title='SSScore %s β' % __VERSION__)
 
-    multiScoresPage = st.Page(score_multilple_jumps, title='Multiple Jumps Set', url_path='jumps_set', icon='🔢')
+#     multiScoresPage = st.Page(score_multilple_jumps, title='Multiple Jumps Set', url_path='jumps_set', icon='🔢')
     singleScorePage = st.Page(score_single_jump, title='Single Jump', url_path='single_jump', icon='1️⃣')
-    pageApp = st.navigation([ multiScoresPage, singleScorePage, ])
+#     pageApp = st.navigation([ multiScoresPage, singleScorePage, ])
+    pageApp = st.navigation([ singleScorePage, ])
     pageApp.run()
 
